351a8964cb02f810b0c1029e8214d7152c152071
Divyesh <tekale2@tamu.edu>
2017-10-06 22:07:45 +0000

    Improved adm fixed rendering error in index page

--- app/assets/javascripts/shoppingStore.js
+++ app/assets/javascripts/shoppingStore.js
@@ -3,7 +3,7 @@
 /*---JQuery Calls---*/
 $(document).ready(function() {
 $( "#searchForm" ).submit(function( event ) {
-
+    event.preventDefault();
     $.ajax({
         type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
         url         : '/show', // the url where we want to POST
@@ -11,16 +11,22 @@ $( "#searchForm" ).submit(function( event ) {
         dataType    : 'json', // what type of data do we expect back from the server
         encode      : true,
     }).done (function(returndata){
+        if ($.fn.DataTable.isDataTable("#tableOutput")) {
+          $('#tableOutput').DataTable().clear().destroy();
+        }
+            var keys = Object.keys(returndata[0]);
+            var columnList = [];
+            for (var i =0;i<keys.length; i++)
+            {
+                var obj = {};
+                obj["data"] = keys[i];
+                columnList.push(obj);
+            }
             $("#tableOutput").DataTable({
             data: returndata,
-            columns: [
-            {data :'id'},
-            {data :'name'},
-            {data :'contact'},
-            {data :'email'}
-            ]
+            columns: columnList
             });
         });
-  event.preventDefault();
+    return false;
 });
 });
